1b71ddedbca4eff167e4e57ad9056a8cb5190ca3
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-02 16:10:41 -0400

    center items

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Navbar() {
 	return (
-		<div className="bg-white w-full px-5 py-5 sm:px-20 sticky top-0 z-1000 mb-4 items-end flex justify-end">
+		<div className="bg-white w-full px-5 py-4 sm:px-20 sticky top-0 z-1000 mb-4 flex justify-end items-center">
 			<div className="mr-auto">
 				<IconButton>
 					<ShoppingCartIcon />
