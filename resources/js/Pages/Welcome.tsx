d9712e1e76a8eb4af71ab95d7125d7a5b0bdb8ab
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-04 19:22:38 -0400

    fix grid style

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -45,7 +45,7 @@ export default function Welcome() {
 							<ProductCard key={item.id} item={item} className="" />
 						),
 						loading: (
-							<div className="my-5 mx-2 grid grid-cols-[repeat(auto-fit,minmax(20rem,_1fr))] gap-4">
+							<div className="my-5 mx-2 grid grid-cols-[repeat(auto-fill,minmax(20rem,_1fr))] gap-4">
 								{[...Array(8).fill(null)].map((_, i) => (
 									<LoadingProductCard key={`card-${i}`} />
 								))}
