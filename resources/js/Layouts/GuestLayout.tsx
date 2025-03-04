cf7423fc87a822b9cec3a331f3052bdbdfcfa63e
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-04 10:20:48 -0400

    change guestLayout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -5,13 +5,15 @@ import type { PropsWithChildren } from "react";
 export default function Guest({ children }: PropsWithChildren) {
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-			<div>
-				<Link href="/">
-					<ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-				</Link>
-			</div>
+			<div className="bg-white p-10 items-center flex flex-col rounded-lg border border-gray-400 w-100 shadow-2xl">
+				<div className="mb-8">
+					<Link href="/">
+						<ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+					</Link>
+				</div>
 
-			{children}
+				{children}
+			</div>
 		</div>
 	);
 }
