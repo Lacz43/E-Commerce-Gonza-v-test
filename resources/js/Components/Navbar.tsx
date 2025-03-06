91376bd1668e0d41fa03b61af70c75caa2363d39
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-06 11:03:21 -0400

    fix z-index

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Navbar() {
 	return (
-		<div className="bg-white w-full px-5 py-2 sm:px-20 sticky top-0 z-1000 mb-4 flex justify-end items-center">
+		<div className="bg-white w-full px-5 py-2 sm:px-20 sticky top-0 z-50 mb-4 flex justify-end items-center shadow-xl">
 			<div className="mr-auto">
 				<IconButton id="shopping-cart">
 					<ShoppingCartIcon />
