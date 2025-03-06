6b3f3ee46d146aa705280caa4b37d7a3dc818573
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-05 23:33:45 -0400

    add fake Products

---
+++ database/factories/ProductsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'image' => 'https://picsum.photos/' . $this->faker->numberBetween(400, 800) . '/' . $this->faker->numberBetween(300, 600),
+            'barcode' => $this->faker->unique()->numberBetween(000000000000000, 9999999999999),
+            'description' => $this->faker->word(),
+            'price' => $this->faker->randomFloat(2, 60, 600),
+        ];
+    }
+}
