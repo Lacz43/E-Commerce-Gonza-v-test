7c1a1fb33e18ee612434c7b4f59c6d0ce944044e
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-14 11:24:53 -0400

    add function to open car

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -2,11 +2,11 @@ import { Link } from "@inertiajs/react";
 import { IconButton } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-export default function Navbar() {
+export default function Navbar({openCar}: {openCar?: () => void}) {
 	return (
 		<div className="bg-white w-full px-5 py-2 sm:px-20 sticky top-0 z-50 mb-4 flex justify-end items-center shadow-xl">
 			<div className="mr-auto">
-				<IconButton id="shopping-cart">
+				<IconButton id="shopping-cart" onClick={() => openCar?.()}>
 					<ShoppingCartIcon />
 				</IconButton>
 			</div>
