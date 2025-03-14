2a7b0d1cd5dddd82b251dee02f606aa2163b58c0
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-14 11:12:13 -0400

    use car modal

--- resources/js/Layouts/EcommerceLayout.tsx
+++ resources/js/Layouts/EcommerceLayout.tsx
@@ -2,11 +2,15 @@ import ApplicationLogo from "@/Components/ApplicationLogo";
 import Navbar from "@/Components/Navbar";
 import { Link } from "@inertiajs/react";
 import type { PropsWithChildren } from "react";
+import Modal from "@/Components/Modal";
+import ProductsInCar from "@/Components/ProductsInCar";
+import { Close, WhatsApp } from "@mui/icons-material";
+import Button from "@mui/material/Button";
 
 export default function Ecommerce({ children }: PropsWithChildren) {
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <Navbar/>
+			<Navbar />
 			<div>
 				<Link href="/">
 					<ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
@@ -14,6 +18,25 @@ export default function Ecommerce({ children }: PropsWithChildren) {
 			</div>
 
 			{children}
+
+			<Modal show={true} onClose={() => console.log("test")} maxWidth="2xl">
+				<div className="border-b border-b-gray-300 py-2 px-4 text-xl bg-gray-100 flex">
+					<h2 className="font-bold">Carrito</h2>
+					<button type="button" className="ml-auto">
+						<Close />
+					</button>
+				</div>
+				<div className="p-3 flex">
+					<ProductsInCar
+						item={{ image: "", name: "test", price: 12 } as Item}
+					/>
+				</div>
+				<div className="border-t border-t-gray-300 py-2 px-4 bg-gray-100">
+					<Button size="medium" variant="contained" endIcon={<WhatsApp />}>
+						<b>Enviar pedido por WhatsApp</b>
+					</Button>
+				</div>
+			</Modal>
 		</div>
 	);
 }
