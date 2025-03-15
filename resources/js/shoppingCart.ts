99f81f9626af3168ac45dd2a9bff67b31f1932c2
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-15 00:05:14 -0400

    add basic shopping cart functions

---
+++ resources/js/shoppingCart.ts
@@ -0,0 +1,58 @@
+const cartId = "shopping_cart";
+
+export default class shoppingCart {
+	public items: Item[];
+	public total: number;
+
+	constructor() {
+		this.items = JSON.parse(sessionStorage.getItem(cartId) || "[]") || [];
+		this.total = 0;
+	}
+
+	add(item: Item): void {
+		const subItem = this.items.find((sub) => sub.id === item.id);
+		if (subItem?.quantity) subItem.quantity ++;
+		else {
+			item.quantity = 1;
+			this.items.push(item);
+		}
+
+		this.save();
+	}
+
+	update(id: number, quantity: number): void {
+		const subItem = this.items.find((sub) => sub.id === id);
+		if (subItem) {
+			if (quantity <= 0) {
+				this.remove(id);
+				return;
+			}
+			subItem.quantity = quantity;
+		}
+		this.save();
+	}
+
+	updateTotal(): void {
+		this.total = this.items.reduce(
+			(sum, current) => sum + current.price * (current.quantity ?? 1),
+			0,
+		);
+	}
+
+	remove(id: number): void {
+		this.items.filter((item) => item.id !== id);
+		this.save();
+	}
+
+	clear(): void {
+		this.items = [];
+		this.total = 0;
+		sessionStorage.clear();
+	}
+
+	save(): void {
+		this.updateTotal();
+		sessionStorage.setItem(cartId, JSON.stringify(this.items));
+		console.log(sessionStorage.getItem(cartId));
+	}
+}
