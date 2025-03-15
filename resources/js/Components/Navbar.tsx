a13f2d7213bb2ab6069575a91b3ca501c54fbbc8
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-15 15:27:44 -0400

    count items in navbar

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -1,13 +1,35 @@
 import { Link } from "@inertiajs/react";
 import { IconButton } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge, { badgeClasses } from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import shoppingCart from "@/shoppingCart";
 
-export default function Navbar({openCar}: {openCar?: () => void}) {
+const CartBadge = styled(Badge)`
+  & .${badgeClasses.badge} {
+    top: -12px;
+    right: -6px;
+  }
+`;
+
+export default function Navbar({ openCar }: { openCar?: () => void }) {
+	const [total, setTotal] = useState(0);
+
+	useEffect(() => {
+		addEventListener("addCart", () => {
+			const cart = new shoppingCart();
+			setTotal(cart.items.length);
+		});
+		return () => removeEventListener("addCart", () => {});
+	}, []);
+
 	return (
 		<div className="bg-white w-full px-5 py-2 sm:px-20 sticky top-0 z-50 mb-4 flex justify-end items-center shadow-xl">
 			<div className="mr-auto">
 				<IconButton id="shopping-cart" onClick={() => openCar?.()}>
 					<ShoppingCartIcon />
+					<CartBadge badgeContent={total} color="primary" overlap="circular" />
 				</IconButton>
 			</div>
 			<ul className="flex gap-4">
