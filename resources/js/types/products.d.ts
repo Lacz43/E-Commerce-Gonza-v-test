0721ac0a1585d222b79de6ba7834cccb47a1c26a
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-15 15:38:52 -0400

    add barcode to Item interface

--- resources/js/types/products.d.ts
+++ resources/js/types/products.d.ts
@@ -2,8 +2,9 @@ declare interface Item<T = unknown> {
 	id: number;
 	name: string;
 	image: string;
+	barcode: number;
 	description: string;
 	price: number;
-    quantity?: number;
+	quantity?: number;
 	data?: T;
 }
