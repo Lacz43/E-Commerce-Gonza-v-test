d3cf8cf34e05e671cdd8305ec5417c3f5280e58a
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-04-06 22:44:20 -0400

    add aside to dashboar layout

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,22 +1,26 @@
 import type { PropsWithChildren, ReactNode } from "react";
 import Navbar from "@/Components/Dashboard/Navbar";
+import Aside from "@/Components/Dashboard/Aside";
 
 export default function Authenticated({
 	header,
 	children,
 }: PropsWithChildren<{ header?: ReactNode }>) {
 	return (
-		<div className="min-h-screen bg-gray-100">
-            <Navbar />
-			{/* {header && ( */}
-			{/* 	<header className="bg-white shadow"> */}
-			{/* 		<div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"> */}
-			{/* 			{header} */}
-			{/* 		</div> */}
-			{/* 	</header> */}
-			{/* )} */}
+		<div className="min-h-screen bg-gray-100 flex">
+			<Aside />
+			<div className="w-full">
+				<Navbar />
+				{/* {header && ( */}
+				{/* 	<header className="bg-white shadow"> */}
+				{/* 		<div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"> */}
+				{/* 			{header} */}
+				{/* 		</div> */}
+				{/* 	</header> */}
+				{/* )} */}
 
-			<main>{children}</main>
+				<main>{children}</main>
+			</div>
 		</div>
 	);
 }
