ee0573c54562ff02290a8fd9083755cf1d03215b
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-04-12 15:32:31 -0400

    add styled buttom

--- resources/js/Components/Dashboard/SideNavItem.tsx
+++ resources/js/Components/Dashboard/SideNavItem.tsx
@@ -2,11 +2,35 @@ import { Link } from "@inertiajs/react";
 import { useEffect, useState } from "react";
 import { paths } from "@/paths";
 
+import { styled } from "@mui/material/styles";
+import Button, { type ButtonProps } from "@mui/material/Button";
+import { blue } from "@mui/material/colors";
+
+interface ColorButtonProps extends ButtonProps {
+	active?: boolean;
+}
+
+const ColorButton = styled(Button)<ColorButtonProps>(({ theme, active }) => ({
+	color: theme.palette.getContrastText(blue[500]),
+	backgroundColor: active ? blue[900] : "#00000000",
+	width: "100%",
+	justifyContent: "start",
+	padding: "0.5rem 1.5rem",
+	boxShadow: "none",
+    textTransform: "none",
+	"&:hover": {
+		backgroundColor: blue[900],
+	},
+    "& .MuiButton-endIcon": {
+        marginLeft: "auto",
+    }
+}));
+
 export default function SideNavItem() {
 	const [active, setActive] = useState("");
 
 	useEffect(() => {
-        // obtner el path actual
+		// obtner el path actual
 		const path = window.location.pathname.split("/").filter(Boolean);
 		setActive(path[0]); // setear el path actual (solo el primer elemento)
 	}, []);
@@ -14,13 +38,14 @@ export default function SideNavItem() {
 	const list = Object.entries(paths).map(([clave, valor]) => {
 		if (valor.hide === true) return;
 		return (
-			<Link
-				key={clave}
-				href={route(valor.path)}
-				className={`flex items-center px-4 py-2 rounded-md ${valor.path === active ? "bg-blue-700" : ""}`}
-			>
-				<div className="pr-2">{valor.icon}</div>
-				{valor.name}
+			<Link key={clave} href={route(valor.path)}>
+				<ColorButton
+					variant="contained"
+					startIcon={valor.icon}
+					active={active === valor.path}
+				>
+					{valor.name}
+				</ColorButton>
 			</Link>
 		);
 	});
