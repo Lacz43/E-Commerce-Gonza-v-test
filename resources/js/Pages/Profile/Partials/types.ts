c09b7cf6f62181c6d3a8c8cdf4e2ad4c975070c3
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-04-11 22:38:56 -0400

    move auth types to user types

--- resources/js/Pages/Profile/Partials/types.ts
+++
@@ -1,3 +0,0 @@
-export interface Auth {
-	auth: { user: User };
-}
--- resources/js/Types/user.d.ts
+++ resources/js/Types/user.d.ts
@@ -3,4 +3,10 @@ declare interface User {
 	name: string;
 	email: string;
 	email_verified_at?: string;
+    roles: string[];
+    permissions: string[];
+}
+
+declare interface Auth {
+	auth: { user: User };
 }
