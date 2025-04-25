abce0a1ff653fdfe853451542a69775489bd792f
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-04-25 17:32:52 -0400

    add permissions seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        $modules = config('modules');
+        $permissions = config('permission.permissions');
+        $roles = config('roles');
+
+        foreach ($permissions as $permission) {
+            foreach ($modules as $module) {
+                Permission::firstOrCreate(['name' => "{$permission} {$module}"]);
+            }
+        }
+
+        foreach ($roles as $roleName => $modules) {
+            $role = Role::firstOrCreate(['name' => $roleName]);
+
+            $rolePermissions = [];
+
+            foreach ($modules as $module) {
+                foreach ($permissions as $permission) {
+                    $rolePermissions[] = "{$permission} {$module}";
+                }
+            }
+
+            $role->syncPermissions($rolePermissions);
+        }
+    }
+}
