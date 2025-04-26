f3186aa7f49e3699b6c66f99d1525188b350d2f9
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-04-26 12:05:33 -0400

    use new usePermission hook

--- resources/js/Components/PermissionGate.tsx
+++ resources/js/Components/PermissionGate.tsx
@@ -2,18 +2,18 @@ import usePermissions from "@/Hook/usePermissions";
 import type { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {
-	permission?: string;
-	role?: string;
+	permission?: string[];
+	roles?: string[];
 }
 
 // componente que verifica si el usuario tiene permiso para ver el contenido
-export default function PermissionGate({ permission, role, children }: Props) {
+export default function PermissionGate({ permission, roles, children }: Props) {
 	const { hasPermission, hasRole } = usePermissions();
 
-	if (!role && !permission) return null;
+	// if (!roles && !permission) return null;
 
-	if (permission && !hasPermission(permission)) return null;
-	if (role && !hasRole(role)) return null;
+	if (permission?.length && !hasPermission(permission)) return null;
+	if (roles?.length && !hasRole(roles)) return null;
 
 	return <>{children}</>;
 }
