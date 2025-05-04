730b06c37490cb642e51b6a74cafc7d13d5badfe
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-04 12:59:55 -0400

    use new style to dataTable

--- resources/js/Components/DataTable.tsx
+++ resources/js/Components/DataTable.tsx
@@ -1,21 +1,55 @@
-import { DataGrid, type GridRowsProp, type GridColDef, type GridValidRowModel } from "@mui/x-data-grid";
+import {
+	DataGrid,
+	type GridRowsProp,
+	type GridColDef,
+	type GridValidRowModel,
+} from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
+import { router } from "@inertiajs/react";
+import { useState } from "react";
 
-const paginationModel = { page: 0, pageSize: 5 };
+const paginationModel = { page: 1, pageSize: 5 };
 
 export type tableProps = {
 	content: GridRowsProp;
 	columns: GridColDef[];
+	rows: number;
+	links: [
+		{
+			url: string | URL;
+			label: string;
+			active: boolean;
+		},
+	];
 };
 
 export type TableContent<T extends GridValidRowModel> = T[];
 
-export default function DataTable({ content, columns }: tableProps) {
+export default function DataTable({
+	content,
+	columns,
+	rows,
+	links,
+}: tableProps) {
+	const [loading, setLoading] = useState(false);
+
 	return (
-		<Paper sx={{ height: 400, width: "100%" }}>
+		<Paper sx={{ width: "100%" }}>
 			<DataGrid
 				rows={content}
 				columns={columns}
+				rowCount={rows}
+				loading={loading}
+				onPaginationModelChange={(model) => {
+					setLoading(true);
+					router.visit(links[model.page].url, {
+						preserveState: true,
+						preserveScroll: true,
+						onFinish: () => {
+							setLoading(false);
+						},
+					});
+				}}
 				initialState={{ pagination: { paginationModel } }}
 				pageSizeOptions={[5, 10, 20]}
 				paginationMode="server"
