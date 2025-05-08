46a8e743c25ec18d832dc13b529102c9bd9417cb
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-08 18:56:27 -0400

    add Users page

---
+++ resources/js/Pages/User/Index.tsx
@@ -0,0 +1,38 @@
+import DataTable from "@/Components/DataTable";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+
+type Props = {
+	users: paginateResponse<User>;
+};
+
+export default function Products({ users }: Props) {
+	return (
+		<AuthenticatedLayout
+			header={
+				<h2 className="text-xl font-semibold leading-tight text-gray-800">
+					Products
+				</h2>
+			}
+		>
+			<Head title="Usuarios" />
+
+			<div className="py-12">
+				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+						<div className="p-6 text-gray-900">
+							<DataTable<User>
+								columns={[
+									{ field: "id", headerName: "ID" },
+									{ field: "name", headerName: "Nombre" },
+									{ field: "email", headerName: "Correo", width: 200 },
+								]}
+								response={users}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</AuthenticatedLayout>
+	);
+}
