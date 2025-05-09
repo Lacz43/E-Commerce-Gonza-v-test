117986486d22af0320512b0d0cebd5a644b580df
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-09 11:50:03 -0400

    relation with roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        $users = User::paginate($request->query("perPage", 20));
+        $users = User::with('roles')->paginate($request->query("perPage", 20));
         return Inertia::render('User/Index', [
             'users' => $users,
         ]);
