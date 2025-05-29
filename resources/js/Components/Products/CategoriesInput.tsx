df1c9a5b008992d97d21fc9a2dad55ad4b5f43e2
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-29 10:49:02 -0400

    add loading to categoriesInput

--- resources/js/Components/Products/CategoriesInput.tsx
+++ resources/js/Components/Products/CategoriesInput.tsx
@@ -8,6 +8,7 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import type { FieldError, UseFormRegisterReturn } from "react-hook-form";
+import CircularProgress from "@mui/material/CircularProgress";
 
 type Props = {
 	errors: FieldError | undefined;
@@ -18,14 +19,17 @@ type Category = { id: number; name: string };
 
 export default function CategoryInput({ errors, register }: Props) {
 	const [categories, setCategories] = useState<Category[]>([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
+		setLoading(true);
 		const fetchData = async () => {
 			try {
 				const { data } = await axios.get<Category[]>(
 					route("products.categories"),
 				);
 				setCategories(data);
+				setLoading(false);
 			} catch (e) {
 				console.log(e);
 			}
@@ -39,7 +43,14 @@ export default function CategoryInput({ errors, register }: Props) {
 			className="mt-3 w-full"
 			required
 			error={errors !== undefined}
+			disabled={loading}
 		>
+			{loading && (
+				<CircularProgress
+					sx={{ position: "absolute", alignSelf: "anchor-center" }}
+					className="right-0 mr-8 "
+				/>
+			)}
 			<InputLabel id="category_product">Categoria</InputLabel>
 			<Select
 				labelId="category_product"
