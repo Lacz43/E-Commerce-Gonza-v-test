fbc582a7ce26b0d0304b47aa679838bd76d1d326
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-29 13:55:20 -0400

    add basic storage info on products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Products;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ProductsController extends Controller
@@ -33,6 +34,30 @@ class ProductsController extends Controller
     public function storage(Request $request)
     {
         Debugbar::info($request);
+        $data = $request->validate([
+            'name' => 'required|string',
+            'barcode' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'category' => 'required|exists:product_categories,id',
+            'description' => 'nullable|string',
+            'images' => 'required|array',
+            'images.*' => ['sometimes', function ($attribute, $value, $fail) {
+                Debugbar::info($value);
+                if (!is_file($value) && !is_string($value)) {
+                    $fail("El elemento {$attribute} debe ser un archivo o una cadena.");
+                }
+            }],
+        ]);
+
+        Products::create([
+            'name' => $data['name'],
+            'barcode' => $data['barcode'],
+            'price' => $data['price'],
+            'category_id' => $data['category'],
+            'description' => $data['description'],
+            'image' => 'http://test.com',
+            'created_by' => Auth::user()->id,
+        ]);
         return json_encode(["test" => "test"]);
     }
 }
