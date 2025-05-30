b58b891eb37fc46b7188f9a28f6ecd22d3d269cf
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-30 12:15:04 -0400

    add default image relation

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Products extends Model
 {
@@ -15,5 +16,10 @@ class Products extends Model
     public function images(): HasMany
     {
         return $this->hasMany(ProductImage::class, 'product_id', 'id');
+    }
+
+    public function defaultImage(): HasOne
+    {
+        return $this->hasOne(ProductImage::class, 'product_id', 'id')->where('default', true);
     }
 }
