8f0f57e02c7702ce2b88d27c5253b87afd826058
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-06-06 18:41:06 -0400

    add basic tooltip check

--- resources/js/Components/Register/PasswordInput.tsx
+++ resources/js/Components/Register/PasswordInput.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { TextField } from "@mui/material";
 import PasswordTooltip from "./PasswordTooltip";
 import type { UseFormRegisterReturn, FieldError } from "react-hook-form";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 
 type Props = {
 	className?: string;
@@ -9,11 +11,39 @@ type Props = {
 	errors: FieldError | undefined;
 };
 
+function Check(checked: boolean) {
+	if (checked) return <CheckIcon />;
+	return <CloseIcon />;
+}
+
 export default function PasswordInput({ className, register, errors }: Props) {
 	const [open, setOpen] = React.useState(false);
 	return (
 		<div className={className}>
-			<PasswordTooltip open={open} setOpen={() => setOpen(false)}>
+			<PasswordTooltip
+				open={open}
+				setOpen={() => setOpen(false)}
+				title={
+					<div>
+						<p>
+							<span>{Check(false)}</span>
+							minimo de 8 caracteres
+						</p>
+						<p>
+							<span>{Check(false)}</span>1 caracter en mayuscula
+						</p>
+						<p>
+							<span>{Check(false)}</span>1 caracter alfa numerico
+						</p>
+						<p>
+							<span>{Check(false)}</span>1 caracter especial
+						</p>
+						<p>
+							<span>{Check(false)}</span>1 caracter en minuscula
+						</p>
+					</div>
+				}
+			>
 				<TextField
 					id="password"
 					label="Contraseña"
