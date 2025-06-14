0705bbaa1bcf2580ed74b32763fbd50e563ea3a6
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-06-14 12:24:21 -0400

    create CrudButton component

---
+++ resources/js/Components/CrudButton.tsx
@@ -0,0 +1,38 @@
+import { IconButton, Tooltip } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ShowIcon from "@mui/icons-material/Delete";
+
+type Props = {
+	type: "show" | "delete" | "edit";
+	onClick?: () => void;
+};
+
+export default function CrudButton({ type, onClick }: Props) {
+	const color = {
+		show: "primary",
+		edit: "primary",
+		delete: "error",
+	} as const;
+
+	const label = {
+		show: "Detalles",
+		edit: "Editar",
+		delete: "Eliminar",
+	} as const;
+	return (
+		<Tooltip title={label[type]}>
+			<IconButton
+				color={color[type] || "primary"}
+				onClick={(e) => {
+					e.stopPropagation();
+					onClick?.();
+				}}
+			>
+				{type === "edit" && <EditIcon />}
+				{type === "delete" && <DeleteIcon />}
+				{type === "show" && <ShowIcon />}
+			</IconButton>
+		</Tooltip>
+	);
+}
