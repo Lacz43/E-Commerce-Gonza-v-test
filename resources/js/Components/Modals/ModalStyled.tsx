3a40aea6b44986c54dbe1d2b398543d928c76807
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-06-25 19:10:59 -0400

    add modal styled

---
+++ resources/js/Components/Modals/ModalStyled.tsx
@@ -0,0 +1,31 @@
+import Modal, { type ModalProps } from "@/Components/Modals/Modal";
+import { JSX } from "react";
+import { Close } from "@mui/icons-material";
+
+type Props = {
+	header: JSX.Element;
+	body: JSX.Element | JSX.Element[];
+	footer: JSX.Element | JSX.Element[];
+};
+
+export default function ModalStyled(
+	props: Props & ModalProps
+) {
+    const { header, body, footer, ...rest } = props;
+	return (
+		<Modal {...rest}>
+			<div className="max-h-dvh flex flex-col">
+				<div className="border-b border-b-gray-300 py-2 px-4 text-xl bg-gray-100 flex justify-between">
+					{header}
+					<button type="button" onClick={() => props.onClose(false)}>
+						<Close />
+					</button>
+				</div>
+				<div className="p-3 overflow-x-hidden overflow-y-scroll">{body}</div>
+				<div className="border-t border-t-gray-300 py-2 px-4 bg-gray-100 md:flex">
+					{footer}
+				</div>
+			</div>
+		</Modal>
+	);
+}
