b82c4d5c74195a52d7154ab04e7dd52e1ca60b10
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-07-11 15:59:40 -0400

    add backup buttom

--- resources/js/Pages/Settings/BackupAndRestore/Index.tsx
+++ resources/js/Pages/Settings/BackupAndRestore/Index.tsx
@@ -1,9 +1,11 @@
-import { Head } from "@inertiajs/react";
+import { Head, router } from "@inertiajs/react";
+import BackupIcon from "@mui/icons-material/Backup";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import type { GridColDef } from "@mui/x-data-grid";
+import axios from "axios";
 import { format } from "date-fns";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useCallback, useState } from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
 const DataTable = lazy(() => import("@/Components/DataTable"));
@@ -21,6 +23,8 @@ type Props = {
 
 export default function BackupAndRestore({ backups }: Props) {
 	console.log(backups);
+    const [loading, setLoading] = useState(false);
+
 	const Columns: GridColDef[] = [
 		{ field: "name", headerName: "Nombre", width: 200 },
 		{
@@ -33,7 +37,7 @@ export default function BackupAndRestore({ backups }: Props) {
 			field: "lastModified",
 			headerName: "Modificado",
 			type: "dateTime",
-            width: 200,
+			width: 200,
 			valueGetter: (value) =>
 				new Date(format(value * 1000, "dd/MM/yyyy HH:mm:ss")),
 		},
@@ -53,6 +57,18 @@ export default function BackupAndRestore({ backups }: Props) {
 			),
 		},
 	];
+
+	const handleBackup = useCallback(async () => {
+	    setLoading(true);
+        try {
+            await axios.post(route("backup.trigger"));
+            setLoading(false);
+            router.reload();
+        } catch (e) {
+            console.log(e);
+        }
+	}, []);
+
 	return (
 		<AuthenticatedLayout
 			header={
@@ -65,6 +81,19 @@ export default function BackupAndRestore({ backups }: Props) {
 			<div className="py-12">
 				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div className="flex justify-end mb-3 mx-3"></div>
+					<div className="bg-white shadow-lg sm:rounded-lg grid grid-cols-2 gap-4 mb-3 p-2">
+						<div className="text-gray-900">
+							<Button
+								variant="contained"
+								size="medium"
+								endIcon={<BackupIcon />}
+                                onClick={handleBackup}
+                                loading={loading}
+							>
+								<b>Respaldar</b>
+							</Button>
+						</div>
+					</div>
 					<div className="overflow-hidden bg-white shadow-lg sm:rounded-lg">
 						<div className="p-6 text-gray-900">
 							<Suspense>
