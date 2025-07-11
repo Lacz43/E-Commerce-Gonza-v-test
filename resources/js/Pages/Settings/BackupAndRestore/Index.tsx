031a4603ded85070133ad174d329ef218806c4b2
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-07-11 15:29:20 -0400

    add backup table

--- resources/js/Pages/Settings/BackupAndRestore/Index.tsx
+++ resources/js/Pages/Settings/BackupAndRestore/Index.tsx
@@ -1,7 +1,50 @@
 import { Head } from "@inertiajs/react";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import { IconButton } from "@mui/material";
+import type { GridColDef } from "@mui/x-data-grid";
+import { lazy, Suspense } from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
-export default function BackupAndRestore() {
+const DataTable = lazy(() => import("@/Components/DataTable"));
+
+type BackupAndRestore = {
+	name: string;
+	size: number;
+	lastModified: string;
+	url: string;
+};
+
+type Props = {
+	backups: paginateResponse<BackupAndRestore>;
+};
+
+export default function BackupAndRestore({ backups }: Props) {
+	console.log(backups);
+	const Columns: GridColDef[] = [
+		{ field: "name", headerName: "Nombre", width: 200 },
+		{
+			field: "size",
+			headerName: "Tamaño",
+			type: "number",
+			valueGetter: (value) => `${(Number(value) / 1024).toFixed(2)} KB`,
+		},
+		{ field: "lastModified", headerName: "Modificado" },
+		{
+			field: "url",
+			headerName: "Acciones",
+			type: "actions",
+			renderCell: (params) => (
+				<div className="flex">
+					<IconButton
+						color="primary"
+						onClick={() => window.open(params.row.url, "_blank")}
+					>
+						<FileDownloadIcon />
+					</IconButton>
+				</div>
+			),
+		},
+	];
 	return (
 		<AuthenticatedLayout
 			header={
@@ -15,7 +58,16 @@ export default function BackupAndRestore() {
 				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div className="flex justify-end mb-3 mx-3"></div>
 					<div className="overflow-hidden bg-white shadow-lg sm:rounded-lg">
-						<div className="p-6 text-gray-900"></div>
+						<div className="p-6 text-gray-900">
+							<Suspense>
+								<DataTable
+									columns={Columns}
+									response={backups}
+									filtersAvailable={false}
+									sortAvailable={false}
+								/>
+							</Suspense>
+						</div>
 					</div>
 				</div>
 			</div>
