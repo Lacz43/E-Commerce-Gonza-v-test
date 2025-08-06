de70651157e7e754b19d440990d701674bf5a57b
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-08-06 12:14:14 -0400

    remove navbar close cart function

--- resources/js/Layouts/EcommerceLayout.tsx
+++ resources/js/Layouts/EcommerceLayout.tsx
@@ -1,16 +1,12 @@
+import { Link } from "@inertiajs/react";
+import type { PropsWithChildren } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import Navbar from "@/Components/Navbar";
-import { Link } from "@inertiajs/react";
-import { useState, lazy, Suspense, type PropsWithChildren } from "react";
-
-const CartModal = lazy(() => import("@/Components/CartModal"));
 
 export default function Ecommerce({ children }: PropsWithChildren) {
-	const [open, setOpen] = useState(false);
-
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-gray-100 sm:justify-center sm:pt-0">
-			<Navbar openCar={() => setOpen(true)} />
+			<Navbar />
 			<div>
 				<Link href="/">
 					<ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
@@ -18,10 +14,6 @@ export default function Ecommerce({ children }: PropsWithChildren) {
 			</div>
 
 			{children}
-
-			<Suspense>
-				<CartModal show={open} setOpen={setOpen} />
-			</Suspense>
 		</div>
 	);
 }
