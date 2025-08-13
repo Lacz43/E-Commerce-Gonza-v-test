92334f5b23ac43c42b9c8a6b4601971f0615372b
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-08-12 21:33:39 -0400

    add role route

--- routes/user.php
+++ routes/user.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth', 'role:admin')->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
+    Route::get('/roles', [UserController::class, 'roles'])->name('roles');
 });
 
 Route::middleware('auth', 'permission:delete users')->delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
