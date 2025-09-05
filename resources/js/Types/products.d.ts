2a3bd0b3ca7ae76c02892544a646129c3ea6f84f
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-09-05 14:40:55 -0400

    feat: add product_inventory field to Item interface for inventory management

--- resources/js/Types/products.d.ts
+++ resources/js/Types/products.d.ts
@@ -3,6 +3,7 @@ declare interface Item<T = unknown> {
 	name: string;
 	images: string[{ id: number; image: string }];
 	default_image?: { id: number; image: string };
+	product_inventory?: { id: number; product_id: number; stock: number };
 	barcode: string;
 	description: string;
 	price: number;
