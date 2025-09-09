fa0dd20d41cfb2055ee6641190af1d0d734b3e4c
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-09-09 10:51:17 -0400

    feat: update Item interface to use structured types for images, brand, and product inventory

--- resources/js/Types/products.d.ts
+++ resources/js/Types/products.d.ts
@@ -1,12 +1,30 @@
 declare interface Item<T = unknown> {
 	id: number | string;
 	name: string;
-	images: string[{ id: number; image: string }];
-	default_image?: { id: number; image: string };
-	product_inventory?: { id: number; product_id: number; stock: number };
+	images: Image[];
+	default_image?: Image;
+	product_inventory?: ProductInventory;
+	brand?: Brand;
 	barcode: string;
 	description: string;
 	price: number;
 	quantity?: number;
 	data?: T;
+}
+
+declare interface Brand {
+	id: number;
+	name: string;
+	created_by: User;
+}
+
+declare interface ProductInventory {
+	id: number;
+	product_id: number;
+	stock: number;
+}
+
+declare interface Image {
+	id: number;
+	image: string;
 }
