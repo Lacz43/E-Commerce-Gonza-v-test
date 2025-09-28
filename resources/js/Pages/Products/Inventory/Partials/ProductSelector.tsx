e4b58df720b7c856580e8145774301678903f902
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-09-28 15:22:08 -0400

    refactor: remove product creation button from product selector component

--- resources/js/Pages/Products/Inventory/Partials/ProductSelector.tsx
+++ resources/js/Pages/Products/Inventory/Partials/ProductSelector.tsx
@@ -1,10 +1,5 @@
-import AddIcon from "@mui/icons-material/Add";
-import { Button } from "@mui/material";
-import Tooltip from "@mui/material/Tooltip";
 import type { FieldValues, Path } from "react-hook-form";
 import SelectProduct from "@/Components/Products/SelectProduct";
-import { useModal } from "@/Context/Modal";
-import CreateForm from "@/Pages/Products/Partials/Form";
 
 type Props<T extends FieldValues> = {
 	name: Path<T>;
@@ -15,25 +10,5 @@ export default function ProductSelector<T extends FieldValues>({
 	name,
 	id,
 }: Props<T>) {
-	const { openModal } = useModal();
-
-	const createProduct = () => {
-		openModal(() => <CreateForm onSubmit={() => {}} />);
-	};
-
-	return (
-		<div className="flex gap-4 w-full">
-			<SelectProduct<T> name={name} id={id} />
-			<Tooltip title="Crear nuevo producto">
-				<Button
-					variant="contained"
-					color="primary"
-					startIcon={<AddIcon />}
-					onClick={createProduct}
-				>
-					Crear
-				</Button>
-			</Tooltip>
-		</div>
-	);
+	return <SelectProduct<T> name={name} id={id} />;
 }
