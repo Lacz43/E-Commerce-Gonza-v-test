cef048a5d540d2780f852476988b408eeefb3ccb
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-10-03 17:38:47 -0400

    fix: update orders index route path to include /index suffix

--- routes/orders.php
+++ routes/orders.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\OrdersController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
+    Route::get('/orders/index', [OrdersController::class, 'index'])->name('orders.index');
 });
 
 Route::post('/order/store', [OrdersController::class, 'store'])->name('order.store');
