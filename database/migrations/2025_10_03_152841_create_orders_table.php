514f16f0220a7549cd712dbc557bbfcc9f18a8bc
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-10-03 16:19:50 -0400

    feat: make user_id nullable in orders table with set null on delete

--- database/migrations/2025_10_03_152841_create_orders_table.php
+++ database/migrations/2025_10_03_152841_create_orders_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
             $table->enum('status', ['cancelled', 'pending', 'expired', 'completed', 'paid'])->default('pending');
             $table->timestamps();
         });
