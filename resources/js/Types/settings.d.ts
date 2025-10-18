a1cc8eaa7d249f9418e023c16061f4c723646523
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-10-17 22:16:35 -0400

    feat: add TypeScript interface for general company settings

---
+++ resources/js/Types/settings.d.ts
@@ -0,0 +1,10 @@
+declare interface GeneralSettings {
+    company_name: string;
+    company_logo: string | null;
+    company_phone: string | null;
+    company_address: string | null;
+    company_rif: string | null;
+    company_email: string | null;
+    currency: string;
+    reference_price: number | null;
+}
