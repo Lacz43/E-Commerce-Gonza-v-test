aec3318be64f14f4e67369867ee04c72946bec10
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-10-20 23:02:44 -0400

    feat: set default payment wait time to 2 hours in order settings

--- app/Settings/OrderSettings.php
+++ app/Settings/OrderSettings.php
@@ -6,7 +6,7 @@ use Spatie\LaravelSettings\Settings;
 
 class OrderSettings extends Settings
 {
-    public ?int $max_payment_wait_time_hours;
+    public ?int $max_payment_wait_time_hours = 2;
 
     public static function group(): string
     {
